e5d9ecc63d056f6e2b27ac8806d3737a2b57a961
Bailey Lissington <lissington4@gmail.com>
2021-10-02 11:28:07 +1300

    ""

--- assignment_1/src/queue.c
+++ assignment_1/src/queue.c
@@ -9,38 +9,71 @@
 #include <assert.h>
 
 #define handle_error_en(en, msg) \
-        do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+    do                           \
+    {                            \
+        errno = en;              \
+        perror(msg);             \
+        exit(EXIT_FAILURE);      \
+    } while (0)
 
-#define handle_error(msg) \
-        do { perror(msg); exit(EXIT_FAILURE); } while (0)
+#define handle_error(msg)   \
+    do                      \
+    {                       \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
 
-typedef struct QueueStruct {
-
+typedef struct QueueStruct
+{
+    void **array;
+    sem_t items;
+    sem_t spaces;
 
 } Queue;
 
+Queue *queue_alloc(int size)
+{
+    // allocate memory for the queue
+    Queue *queue = malloc(sizeof(Queue));
+    queue->array = malloc(size * sizeof(void *));
 
-Queue *queue_alloc(int size) {
-    assert(0 && "not implemented yet!");
+    // initiliase items semaphore with 0
+    if (sem_init(&queue->items, 0, 0) == -1)
+        handle_error("sem_init items");
 
+    // initialise spaces semaphore with the number of free spaces
+    if (sem_init(&queue->spaces, 0, size) == -1)
+        handle_error("sem_init spaces");
+
+    return queue;
 }
 
-void queue_free(Queue *queue) {
+void queue_free(Queue *queue)
+{
+    free(queue->array);
+    free(queue);
+}
+
+void queue_put(Queue *queue, void *item)
+{
+    // TODO: mutex?
+
+    if (sem_wait(&queue->spaces) == -1)
+        handle_error("sem_wait spaces");
+
+    // Get the number of items in queue
+    int num_items;
+    if (sem_getvalue(&queue->items, &num_items) == -1)
+        handle_error("sem_getvalue items");
+
+    // add the item to the array
+    queue->array[num_items] = item;
+    if (sem_post(&queue->items) == -1)
+        handle_error("sem_post items");
+}
+
+void *queue_get(Queue *queue)
+{
 
     assert(0 && "not implemented yet!");
 }
-
-void queue_put(Queue *queue, void *item) {
-
-    assert(0 && "not implemented yet!");
-
-}
-
-
-
-void *queue_get(Queue *queue) {
-
-    assert(0 && "not implemented yet!");
-
-}
-
--- assignment_1/test/queue_test.c
+++ assignment_1/test/queue_test.c
@@ -4,66 +4,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #include "queue.h"
 
 #define NUM_THREADS 16
 #define N 1000000
 
-typedef struct {
+typedef struct
+{
     int value;
 } Task;
 
-
-void *doSum(void *arg) {
+// Sum every element in the queue until NULL is encountered. Returns the computed sum
+void *doSum(void *arg)
+{
     int sum = 0;
-    Queue *queue = (Queue*)arg;
+    Queue *queue = (Queue *)arg;
 
-    Task *task = (Task*)queue_get(queue);
-    while (task) {
+    Task *task = (Task *)queue_get(queue);
+    while (task)
+    {
         sum += task->value;
         free(task);
 
-        task = (Task*)queue_get(queue);
+        task = (Task *)queue_get(queue);
     }
 
-    pthread_exit((void*)(intptr_t)sum);
+    pthread_exit((void *)(intptr_t)sum);
 }
 
-
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     int i, sum;
 
     pthread_t thread[NUM_THREADS];
     Queue *queue = queue_alloc(NUM_THREADS);
 
-
-    for (i = 0; i < NUM_THREADS; ++i) {
+    for (i = 0; i < NUM_THREADS; ++i)
+    {
         pthread_create(&thread[i], NULL, doSum, queue);
     }
 
     int expected = 0;
-    for (i = 0; i < N; ++i) {
-        Task *task = (Task*)malloc(sizeof(Task));
+    for (i = 0; i < N; ++i)
+    {
+        Task *task = (Task *)malloc(sizeof(Task));
         task->value = i;
-
-
 
         queue_put(queue, task);
         expected += i;
     }
 
-
-    for (i = 0; i < NUM_THREADS; ++i) {
+    // Appends NULL entries for the number of threads
+    for (i = 0; i < NUM_THREADS; ++i)
+    {
         queue_put(queue, NULL);
     }
 
     intptr_t value;
     sum = 0;
-    for (i = 0; i < NUM_THREADS; ++i) {
-        pthread_join(thread[i], (void**)&value);
+    for (i = 0; i < NUM_THREADS; ++i)
+    {
+        pthread_join(thread[i], (void **)&value);
         sum += value;
     }
 
