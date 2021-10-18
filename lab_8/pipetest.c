169cc373d38dc31d02112ea549eda91827416ef3
Bailey Lissington <lissington4@gmail.com>
2021-10-18 21:38:53 +1300

    pipetest

--- lab_8/pipetest.c
+++ lab_8/pipetest.c
@@ -8,8 +8,9 @@
 int main(void)
 {
     int p[2];
+    int p2[2];
 
-
+    pipe(p2);
     pipe(p);
     int pid = fork();
 
@@ -19,6 +20,12 @@ int main(void)
         close(0);
         dup(p[0]);
 
+        // redirect stdout
+        close(1);
+        dup(p2[1]);
+
+        close(p2[0]);
+        close(p2[1]);
         close(p[0]);
         close(p[1]);
 
@@ -31,6 +38,20 @@ int main(void)
 
         close(p[0]);
         close(p[1]);
+
+        close(p2[1]);
+
+        char buf[20];
+        int n;
+        int i = 0;
+        
+        do {
+            n=read(p2[0], buf+i++, 1);
+        } while(n>0);
+
+
+        printf(1, "%s", buf);
+        close(p2[0]);
 
         wait();
     }
